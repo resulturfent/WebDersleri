6e9d650c1ffb8c5a2ca249546f6f42c2d7b2509c
resulturfent <rsltrfnt@gmail.com>
2026-05-22 19:57:10 +0300

    Kategor tablosu migration işlemi

--- DLL/Configurations/KategoriConfiguration.cs
+++ DLL/Configurations/KategoriConfiguration.cs
@@ -1,5 +1,16 @@
-﻿namespace DLL.Configurations;
+﻿using DLL.EntitiesTablolar;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-internal class KategoriConfiguration
+namespace DLL.Configurations;
+
+internal class KategoriConfiguration : IEntityTypeConfiguration<Kategoriler>
 {
+    public void Configure(EntityTypeBuilder<Kategoriler> builder)
+    {
+        
+        builder.HasKey(x => x.Id);//PK
+        builder.Property(x => x.Id).UseIdentityColumn();//SQL de Increment, Seed ayarlarını 1,1 yapar
+        builder.Property(x => x.Adi).IsRequired().HasMaxLength(250);
+    }
 }
