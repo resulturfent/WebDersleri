b4dbd123e6d51ef046d92e8c342227d5ea6a3d1a
resulturfent <rsltrfnt@gmail.com>
2026-06-02 21:34:58 +0300

    Kategori için list table eklemesi yapıldı

---
+++ UI.WEB/Controllers/KategoriController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.WEB.Controllers;
+
+public class KategoriController : Controller
+{
+
+    [HttpGet]
+    public IActionResult List()
+    {
+        return View();
+    }
+
+
+    [HttpGet]
+    public IActionResult Ekle()
+    {
+        return View();
+    }
+}
