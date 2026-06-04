da3f4fc676dbe437fde026adcfd579fbdc958859
resulturfent <rsltrfnt@gmail.com>
2026-06-04 21:01:46 +0300

    EF, Linq, Code First tanımlaması

--- Accesss/Repositories/GenericRepository.cs
+++ Accesss/Repositories/GenericRepository.cs
@@ -26,7 +26,9 @@ public class GenericRepository<TEntityTablo> : IGenericRepository<TEntityTablo> 
 
     public List<TEntityTablo> HepsiniListele()
     {
-        var list = _hepsiNerdeDB.Set<TEntityTablo>().ToList();
+        var list = _hepsiNerdeDB.Set<TEntityTablo>().ToList();//linq ile yazılmış kod yapısıdır. LINQ=> C# kodu içinde veriatabanı kodlarını(SQL)  kodlamaya yarayan bir teknolojidir
+        //Migration işlemi Code-First için kullandığımız bir tabirdir. Code First bir ORM (Object Relation Mapping(Manager)) aracıdır. Ado.Net, Code Model, Db Model, Code First ORM araçlarıdır
+
         return list;
     }
 
